f955fe5fc3125ce2a51e5e85ef8b7e38fd970fd4
vasyaboychuk <boychuk98vasya@gmail.com>
2022-10-24 01:25:51 +0300

    add genre filter

--- src/components/FavouriteMoviesList/FavouriteMoviesList.js
+++ src/components/FavouriteMoviesList/FavouriteMoviesList.js
@@ -1,13 +1,21 @@
 import {FavouriteMovies} from "../FavouriteMovies/FavouriteMovies";
+import css from './FavouriteMovieList.module.css'
 
 function FavouriteMoviesList(props) {
     const movies = JSON.parse(localStorage.getItem('favourite'));
 
     console.log(movies);
     return (
-        <div>
-            {movies.map(movie=><FavouriteMovies key={movie.id} movie={movie}/>)}
+        <div className={css.container}>
+            <div className={css.info}>
+                <h3>Favourite movies</h3>
+
+            </div>
+            <div className={css.wrap}>
+                {movies.map(movie=><FavouriteMovies key={movie.id} movie={movie}/>)}
+            </div>
         </div>
+
     )
 }
 
